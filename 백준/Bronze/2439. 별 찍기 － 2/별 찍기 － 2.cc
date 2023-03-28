0fe326487f0bed2815a51c011a0107b1baf9d69e
Daero <116069354+parkjuneseong@users.noreply.github.com>
2023-03-28 21:47:59 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+  
+   int n;
+   cin >> n;
+   
+   for (int row=1; row<=n; row++){
+       
+      for (int i=0; i < n - row ; i++){
+          
+cout << " ";}
+
+
+for (int i = 0; i < row ; i++)
+    
+cout 
+<< "*";
+
+
+
+cout << "\n";
+}
+   
+  return 0; }
