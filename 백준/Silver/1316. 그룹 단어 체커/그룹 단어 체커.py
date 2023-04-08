14c17e1e2d56c1b07d0e144d13fe311ddaeb8416
Daero <116069354+parkjuneseong@users.noreply.github.com>
2023-04-08 15:52:44 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,11 @@
+t = int(input())
+cnt = t
+for i in range(t):
+    word = input()
+    for j in range(0,len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+print(cnt)
