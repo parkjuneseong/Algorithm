97c00311453c66faae4e73f617c5bcfc078e686e
Daero <116069354+parkjuneseong@users.noreply.github.com>
2023-04-16 21:40:09 +0900

    [Bronze II] Title: 소수, Time: 560 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,17 @@
+m = int(input())
+n = int(input())
+lis = []
+for i in range(m,n+1):
+    count = 0 
+    if i > 1 :
+        for j in range(2,i):
+            if i%j == 0:
+                count +=1
+                break
+        if count == 0:
+            lis.append(i)
+if len(lis)<1:
+    print(-1)
+else:
+    print(sum(lis))
+    print(min(lis))
