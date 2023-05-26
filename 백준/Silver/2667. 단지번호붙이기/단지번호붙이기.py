b73bb458257050615aaf48e9ba4cd8a12a06f9a7
Daero <116069354+parkjuneseong@users.noreply.github.com>
2023-05-26 17:22:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31364 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -8,16 +8,16 @@ def dfs(x,y):
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0<=nx<n and 0<=ny<n and graph[nx][ny] == 1:
+        if 0<=nx < n and 0<=ny < n and graph[nx][ny] == 1 :
             count += dfs(nx,ny)
     return count
 for i in range(n):
-    apart  = list(map(int,input()))
-    graph.append(apart)
+    li = list(map(int,input()))
+    graph.append(li)
 ans = []
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 1 :
+        if graph[i][j]  == 1 :
             ans.append(dfs(i,j))
 ans.sort()
 print(len(ans),*ans,sep='\n')
